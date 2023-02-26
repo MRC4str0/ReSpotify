e52c5b9257be9a0c5ea7a97abf13b1507e8f6eb3
MRC@str0 <68367411+MRC4str0@users.noreply.github.com>
2023-02-26 20:52:20 -0300

    Update Secrets.py

--- Secrets.py
+++ Secrets.py
@@ -1,2 +1,2 @@
-cred = {'client_id':'caaebd559d4049308aa334987da95d36',
-        'client_secret':'50417a4d9d724e3090d3f3c6e533482e',}
+cred = {'client_id':'Seu Client Id',
+        'client_secret':'Seu Client Secret',}
